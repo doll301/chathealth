5a9a7f30607bb0769de15ccc5670dd6dc518bbd5
neilmhta <neilmhta@gmail.com>
2022-01-07 23:51:05 -0500

    created drawer menu used material icons

--- App.js
+++ App.js
@@ -1,5 +1,13 @@
 import React from "react";
-import { StyleSheet } from "react-native";
+import {
+  Text,
+  View,
+  TouchableOpacity,
+  StyleSheet,
+  Image,
+  Button,
+} from "react-native";
+import 'react-native-gesture-handler';
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
@@ -8,10 +16,33 @@ import BotScreen from "./screens/BotScreen.js";
 import HomeScreen from "./screens/HomeScreen.js";
 import NewEventScreen from "./screens/NewEventScreen";
 
+import { createDrawerNavigator } from '@react-navigation/drawer';
+
+import { DrawerContent } from './components/drawercontent';
+
 {
   /* Initializes component we can use to enable navigation between multiple screens. */
 }
 const Stack = createNativeStackNavigator();
+const Drawer = createDrawerNavigator();
+
+
+function Dashboard() {
+  return (
+    <Stack.Navigator  screenOptions={{
+    headerShown: true
+  }}>
+      <Stack.Screen name="Home" component={HomeScreen} />
+      <Stack.Screen name="Events" component={EventScreen} />
+      <Stack.Screen name="ChatBot" component={BotScreen} />
+      <Stack.Screen name="NewEvent" component={NewEventScreen} />
+    </Stack.Navigator>
+
+  );
+}
+
+
+
 
 {
   /* This is the entry point of our app!! */
@@ -19,12 +50,12 @@ const Stack = createNativeStackNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Events" component={EventScreen} />
-        <Stack.Screen name="ChatBot" component={BotScreen} />
-        <Stack.Screen name="NewEvent" component={NewEventScreen} />
-      </Stack.Navigator>
+      <Drawer.Navigator
+      screenOptions={{  headerShown: false}}
+      drawerContent={props => <DrawerContent {... props} />}
+      >
+        <Drawer.Screen name="Dashboard" component={Dashboard} />
+      </Drawer.Navigator>
     </NavigationContainer>
   );
 }
