8955cb91c385506ad97efae8b0c1a506236ac021
neilmhta <neilmhta@gmail.com>
2022-01-20 01:56:21 -0500

    turn off yellowbox

--- App.js
+++ App.js
@@ -40,6 +40,9 @@ import PhysicalScreen from "./screens/topics/PhysicalHealth";
 import SexualScreen from "./screens/topics/SexualHealth";
 import UrgentCareScreen from "./screens/topics/UrgentCare";
 
+import { LogBox } from 'react-native';
+LogBox.ignoreLogs(['Warning: ...']); // Ignore log notification by message
+LogBox.ignoreAllLogs();//Ignore all log notifications
 
 
 
